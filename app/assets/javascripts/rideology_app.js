31315da0c5acccdc083b73bc760b49c313186b1a
Ben Miller <bnmiller@gmail.com>
2014-01-16 09:32:24 -0800

    removed alert

--- app/assets/javascripts/rideology_app.js
+++ app/assets/javascripts/rideology_app.js
@@ -4,7 +4,6 @@ window.RideologyApp = {
   Views: {},
   Routers: {},
   initialize: function() {
-	  alert("For reals");
 	  // RideologyApp.rideOffers = new RideologyApp.Collections.RideOffers();
  // 	  new RideologyApp.Routers.Router({
  // 		  $rootEl: $('.bbhandle')
